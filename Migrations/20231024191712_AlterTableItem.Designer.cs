0e76975af40959c89cd8221e5a235ff29e132f2f
BRUNO GUIMARÃES <bruno.tavares@portalsesisp.org.br>
2023-10-24 16:53:12 -0300

    DTOs

--- Migrations/20231024191712_AlterTableItem.Designer.cs
+++ Migrations/20231024191712_AlterTableItem.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SporArt.Data;
 
@@ -10,9 +11,11 @@ using SporArt.Data;
 namespace SporArt.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231024191712_AlterTableItem")]
+    partial class AlterTableItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20231024191712_AlterTableItem.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SporArt.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTableItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Categoria",
+                table: "Itens");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoriaId",
+                table: "Itens",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Itens_CategoriaId",
+                table: "Itens",
+                column: "CategoriaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Itens_Categorias_CategoriaId",
+                table: "Itens",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Itens_Categorias_CategoriaId",
+                table: "Itens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Itens_CategoriaId",
+                table: "Itens");
+
+            migrationBuilder.DropColumn(
+                name: "CategoriaId",
+                table: "Itens");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Categoria",
+                table: "Itens",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
